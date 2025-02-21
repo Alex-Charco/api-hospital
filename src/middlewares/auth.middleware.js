12b0205515166bb5d2e92aae7af9b725bd1733ea
Alex-Charco <awcharco@utpl.edu.ec>
2025-02-21 16:02:21 -0500

    Eliminar código de mantenibilidad

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,6 @@ const RolUsuario = require("../models/rolUsuarioModel");
 // Middleware para autenticar el token
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    //const token = authHeader && authHeader.split(' ')[1]; 
     const token = authHeader?.split(' ')[1];
 
     if (!token) {
