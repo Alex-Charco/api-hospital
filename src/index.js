bd27f30dc86850194d93c55ee64c71a99a671afd
Alex-Charco <awcharco@utpl.edu.ec>
2025-02-21 15:33:07 -0500

    corrección de errores en sonarcloud

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const cors = require("cors");
 const sequelize = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 
+const corsOptions = {
+    origin: 'http://localhost:5000'
+};
 const app = express();
 
+// Deshabilitar la cabecera "x-powered-by"
+app.disable("x-powered-by");
+
 // 📌 Middlewares
-app.use(cors());
+//app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // 📌 Rutas
