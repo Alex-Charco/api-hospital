1ada2d7d128f2e1d9b3c4d3dc860b4d8ccd4ad04
Alex-Charco <awcharco@utpl.edu.ec>
2025-02-21 15:52:44 -0500

    Correción de código para solucionar error por prueba

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,8 @@ const RolUsuario = require("../models/rolUsuarioModel");
 // Middleware para autenticar el token
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; 
+    //const token = authHeader && authHeader.split(' ')[1]; 
+    const token = authHeader?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ message: 'Acceso denegado: token no proporcionado' });
