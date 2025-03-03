8889532957365fb6ed64be8143e81adf99b20f35
Alex-Charco <awcharco@utpl.edu.ec>
2025-03-03 00:23:06 -0500

    Verificcación que se esta utilizando mysql2

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
@@ -8,6 +9,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectModule: mysql2,
     logging: false,
   }
 );
