4e080ecc588bbc5169e188ffb5f79161ae3db379
Alex-Charco <awcharco@utpl.edu.ec>
2025-03-09 13:54:34 -0500

    Eliminación de código comentado

--- src/routes/turno.routes.js
+++ src/routes/turno.routes.js
@@ -7,28 +7,3 @@ const { verificarToken, authorizeRole } = require('../middlewares/auth.middlewar
 router.get("/get", verificarToken, authorizeRole(["ver_turno"]), getTurnos);
 
 module.exports = router;
-
-
-
-/*const express = require('express');
-const router = express.Router();
-const { getTurnosDisponibles, getTurnosPorFecha  } = require('../controllers/turno.controller');
-const { verificarToken, authorizeRole } = require('../middlewares/auth.middleware');
-
-router.get("/get/disponibles", verificarToken, authorizeRole(["ver_turno"]), getTurnosDisponibles);
-router.get("/get/fecha/:fecha", verificarToken, authorizeRole(["ver_turno"]), getTurnosPorFecha);
-
-module.exports = router;
-
-*/
-/*
-const express = require('express');
-const router = express.Router();
-const { getTurnos } = require('../controllers/turno.controller');
-const { verificarToken, authorizeRole } = require('../middlewares/auth.middleware');
-
-router.get("/get/disponibles", verificarToken, authorizeRole(["ver_turno"]), getTurnos);
-router.get("/get/fecha/:fecha", verificarToken, authorizeRole(["ver_turno"]), getTurnos);
-
-module.exports = router;
- */
